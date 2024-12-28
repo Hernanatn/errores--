17b5a1b7a2a6b35cf37685fa92d36c4ece94d4ad
Herni <hernanatn@gmail.com>
2024-12-27 22:03:02 -0300

    <PRUEBA/> prueba resultados de github actions en pruebas

--- pruebas/pruebas.cpp
+++ pruebas/pruebas.cpp
@@ -272,6 +272,7 @@ TEST_CASE("Resultado con shared_ptr", "[resultado][smart_ptr]") {
         auto [ptr, error] = resultado();
         REQUIRE(error);
         REQUIRE(ptr == nullptr);
+        REQUIRE(1 == 0);
     }
 }
 
