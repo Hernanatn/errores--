4ec5d2a83e0f455e74f0af97c3c81c84dbf87795
Herni <hernanatn@gmail.com>
2024-12-30 01:19:25 -0300

    build > compilar

--- pruebas/compilar_pruebas.ps1
+++ pruebas/compilar_pruebas.ps1
@@ -2,9 +2,9 @@ param(
     [switch]$correr = $false
 )
 cmake -S . -B compilar;
-cmake --build compu;
+cmake --build compilar;
 Remove-Item -Path "pruebas/pruebas.exe";
-Move-Item -Path "build/correr_pruebas.exe" -Destination "pruebas/pruebas.exe";
+Move-Item -Path "compilar/correr_pruebas.exe" -Destination "pruebas/pruebas.exe";
 
 if($correr){
 pruebas/pruebas.exe -s
