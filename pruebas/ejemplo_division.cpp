839cd1b103de803fa68e550795130a17ab3f8634
Herni <hernanatn@gmail.com>
2025-01-14 15:57:41 -0300

    pruebas automatica intento 90004 -> clang en vez de msvc

--- pruebas/ejemplo_division.cpp
+++ pruebas/ejemplo_division.cpp
@@ -1,7 +1,6 @@
 #pragma once
 #include "errores--.hpp"
 #include <iostream>
-
 res::Resultado<int> dividir(int a, int b) {
     if (b == 0) {
         return res::Resultado<int>(0, err::ERROR, "No se puede dividir por cero");
